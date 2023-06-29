37e1096f3a356e712de2f06fc41b46fffff22fe2
StephenBernard98 <stephenbenard3@gmail.com>
2023-06-29 11:21:44 +0100

    pushing the commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import BannerDesign from './body/BannerDesign';
+import Items from './body/Items';
+import Ads from './body/ads/Ads';
+import Terms from './footer/Terms';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BannerDesign />
+      <Items />
+      <Ads />
+      <Terms />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
